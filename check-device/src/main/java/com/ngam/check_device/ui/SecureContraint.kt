2110041457319d50daf4ba79f0e70cb5ed529a38
Manuel-ANG <34840582+Manuel-ANG@users.noreply.github.com>
2023-11-21 15:38:35 -0600

    cambios
    cambios

--- check-device/src/main/java/com/ngam/check_device/ui/SecureContraint.kt
+++ check-device/src/main/java/com/ngam/check_device/ui/SecureContraint.kt
@@ -3,13 +3,16 @@ package com.ngam.check_device.logic.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import androidx.annotation.Keep
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class SecureContraint :ConstraintLayout{
+@Keep
+class SecureContraint : ConstraintLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
     init {
         this.filterTouchesWhenObscured = true
     }
