02a935c5f75bea27c389e62ca4dd4ce61b11b9bc
Manuel-ANG <34840582+Manuel-ANG@users.noreply.github.com>
2023-11-21 14:21:09 -0600

    cambios de seguridad
    Constraint seguro

---
+++ check-device/src/main/java/com/ngam/check_device/logic/ui/SecureContraint.kt
@@ -0,0 +1,26 @@
+package com.ngam.check_device.logic.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SecureContraint :ConstraintLayout{
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    init {
+        this.filterTouchesWhenObscured = true
+    }
+
+    override fun onFilterTouchEventForSecurity(event: MotionEvent?): Boolean {
+        val flags = event?.flags
+        val bad = (((flags?.and(MotionEvent.FLAG_WINDOW_IS_OBSCURED)) != 0) or ((flags?.and(MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED) != 0)))
+        return if (bad) {
+            false
+        } else {
+            return super.onFilterTouchEventForSecurity(event)
+        }
+    }
+}
