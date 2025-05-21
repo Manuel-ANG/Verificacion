fdb5638d9f26ab451f5a57334025a9ef326bc3a2
Manuel-ANG <34840582+Manuel-ANG@users.noreply.github.com>
2025-05-21 13:26:02 -0600

    fixed

--- app/src/main/java/com/ngam/testverificacion/MainActivity.kt
+++ app/src/main/java/com/ngam/testverificacion/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.ngam.testverificacion
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.ngam.check_device.logic.CheckDevice
 import com.ngam.check_device.tap.HideOverlay
 import com.ngam.testverificacion.Logic.Validation
@@ -53,5 +55,8 @@ fun Greeting() {
     } catch (e: Throwable) {
         e.printStackTrace()
     }
-    Text(text = text.value)
+    Column(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+        Text(text = text.value)
+        Text(text = BuildConfig.BASE_URL)
+    }
 }
